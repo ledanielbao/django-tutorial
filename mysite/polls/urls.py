3dd14cb8b026846be748609704e15172715ab1f9
Daniel Le <ledanielbao@gmail.com>
2021-07-06 12:21:19 -0700

    django testing

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,9 +1,15 @@
+from django.contrib import admin
 from django.urls import path
-from . import views 
+from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name = "index"),
-    path('<int: question_id>/', views.detail, name='detail'),
-    path('<int: question_id>/results', views.results, name='results'),
-    path('<int: question_id>/vote/', views.vote, name='vote'),
+    # ex: /polls/
+    path('', views.IndexView.as_view(), name='index'),
+    # ex: /polls/5/
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    # ex: /polls/5/results/
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/votes/', views.vote, name='vote'),
 ]
